37122414ad6cd1d552aa5aa76b56c29662b9e480
Veikko Eeva <veikko.eeva@lumoin.com>
2024-04-02 23:48:05 +0300

    Initial commit

---
+++ Verifable/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Verifable
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
